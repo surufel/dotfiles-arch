ba0158f4de4f4b9d2c36e7d5459ff72a7dec8843
surufel <henriquefrancasmm@gmail.com>
2026-02-08 14:09:42 -0300

    Select-wall script translated to english

--- .config/hypr/scripts/select-wall.sh
+++ .config/hypr/scripts/select-wall.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 
-# Configurações
+# Configurations
 WALLPAPER_DIR="$HOME/Pictures/Wallpapers"
-MONITOR="eDP-1"
+MONITOR="eDP-1" 
+# Change MONITOR in case it is the wrong monitor config
 
-# Verifica a existencia do diretorio
+# Verifies if the directory exists
 if [[ ! -d "$WALLPAPER_DIR" ]]; then
-    echo "Erro: Diretório não encontrado: $WALLPAPER_DIR" >&2
+    echo "Error: Directory not found: $WALLPAPER_DIR" >&2
     exit 1
 fi
 
-# Lista imagens formatadas para o Rofi com ícones
+# Shows the wallpaper images when opening rofi with wallpaper selector
 function list_images() {
     find "$WALLPAPER_DIR" -maxdepth 1 -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) | while read -r path; do
         echo -en "$(basename "$path")\0icon\x1f$path\n"
     done
 }
 
-# Seleção via Rofi
+# Selecting wallpaper via rofi
 selected=$(list_images | rofi -dmenu -i -p "Wallpaper" -show-icons -theme-str 'listview { lines: 5; }')
 
 if [[ -n "$selected" ]]; then
     wallpaper_path="$WALLPAPER_DIR/$selected"
 
 	if awww img "$wallpaper_path" --transition-type grow --transition-pos bottom-right --transition-duration 1 --transition-fps 60; then
-        echo "[+] Wallpaper alterado."
+        echo "[+] Wallpaper changed."
         exit 0
     else
-        echo "[-]: O awww falhou." >&2
+        echo "[-]: awww has failed." >&2
         exit 1
     fi
 fi
